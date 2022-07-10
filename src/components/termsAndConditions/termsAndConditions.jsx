4ad411d7ac119d6a11b6dc699ddaef976bdf6126
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-07-09 18:12:54 -0600

    Add main text to page

--- src/components/termsAndConditions/termsAndConditions.jsx
+++ src/components/termsAndConditions/termsAndConditions.jsx
@@ -3,9 +3,24 @@ import
 
 class TermsAndConditions extends Component {
   render() { 
-    return <h1>...Terms, conditions & other not fun stuff... </h1>;
-    )
-  }
+    return(
+      <div>
+        <h1>...Terms, conditions & other not fun stuff... </h1>
+        <div>
+        Prices are for Denver Colorado, metro area only. Longmont to Lone Tree, Aurora to Arvada.
+        $100+ additional travel fee for outside metro area based on distance.
+        50% minimum deposit required to reserve date. Full payment due 1 week (7 days) prior to party/gathering date.
+        Cancellations must be 1 week (7 days) prior to reserved party/gathering date for deposit refund lees transaction fee of $20. 
+        $100 fee for cancellations that occur less than 7 days prior to the reserved party/gathering date. No refunds for printed invites
+        or personalized ornaments or items.  
+        No refunds once party/gathering services have been rendered. No refunds for guests that do not show up.
+        All guests and Hosts are entitled to enjoy all paid for services up to the afore purchased guest amount. Payment for any additional
+        guests will be paid by Host/Hostess and collected for prior to the start of the party/gathering. 
+        Guest receiving services must be 17 years of age or older. No exceptions. Gypsy Vibes is not appropriate for 
+        </div>
+      </div>
+    );
+    }
 }
  
 export default TermsAndConditions;
