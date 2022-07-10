ab3b1e46e120f2d07948d9dce342718e8c63c820
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-07-09 18:01:10 -0600

    Add base of terms and conditions page

---
+++ src/components/termsAndConditions/termsAndConditions.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import 
+
+class TermsAndConditions extends Component {
+  render() { 
+    return <h1>...Terms, conditions & other not fun stuff... </h1>;
+    )
+  }
+}
+ 
+export default TermsAndConditions;
