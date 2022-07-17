4682fb0fe5edd87c4a20f755e52952060b4ccaba
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-07-17 12:18:21 -0600

    loop through imported terms

---
+++ src/components/termsAndConditionsPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './termsAndConditionsPage.css';
+import rules from '../assets/rules.js';
+
+class TermsAndConditions extends Component {
+  render() {
+    return (
+      <div>
+        <div class='titleHeader'>
+          .....Terms, conditions & other not fun stuff.....
+        </div>
+        <ul class='termsList'>
+          {rules.map(rule => (
+            <li key={rule.id} class='termsText'>
+              {rule.rule}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TermsAndConditions;
