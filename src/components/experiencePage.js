a3f3c58523b3b837668191079d338b956d50b8a1
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-07-20 21:04:28 -0600

    Add experience text

---
+++ src/components/experiencePage.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './experience.css';
+
+class Experience extends Component {
+  render() {
+    return (
+      <div>
+        <div className='titleHeader'>.....What to expect in your session.....</div>
+        <div className='experienceText'>
+          <p>
+            Start your Reiki session with a chakra scan to help determine
+            unbalanced areas within your physical being. Then relax as
+            <b> Christina</b>, with or without touch, gently manipulates your
+            energy to promote healing, relaxation, and wellbeing. After your
+            Energy Healing, you will do another scan. You will then be able to
+            compare your scans and see the benefits of your session.
+          </p>
+          <p>
+            The universe speaks to all. Too bad these messages are not always
+            clear. Maybe, a reoccurring dream has you frustrated, another
+            message that seems to be in a cryptic language. Insight through
+            Tarot, Oracle, palm reading or dream interpretation can help resolve
+            those questions you have about certain aspects of your life. Your
+            session with
+            <b> Stephanie</b> is kept completely confidential, and you will be
+            enlightened to what your universe wants you to know.
+          </p>
+          <p>
+            Whether you are a novice, or an aspiring yogi, <b>Vincent</b>'s
+            specialized Yoga session is catered to you. Gain insight to the
+            beneficial influence of meditation breathing techniques. Learn poses
+            that assist in overall wellness and will promote positivity and
+            fluidness within your mind and body. This one-on-one session is
+            designed to leave you feeling centered and balanced with new
+            knowledge for maintaining the positive vibes you have experienced.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Experience;
