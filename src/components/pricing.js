2d59fbe7ca4ab190fe89ee95589bc9403864be2e
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-07-26 13:41:17 -0600

    Fix left side of page

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,31 +1,67 @@
 import React, { Component } from 'react';
 import pricingInfo from '../assets/pricingInfo';
 import './pricing.css';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 class PricingPage extends Component {
   render() {
     return (
-      <div>
-        <div className='titleHeader'>Pricing.....</div>
-        <div className='subTitle'>Private Event Only </div>
-        <div>(Includes personalized digital invite)</div>
-        <ul className='conatinerRight'>
-          {pricingInfo.map((info) => (
-            <ul key={info.id}>
-              <li>title{info.title}</li>
-              {info.info.map((i) => (
-                <ul>
-                  <li>subTitle: {i.subTitle} </li>
-                  {i.details.map((deets) => (
-                    <li>{deets}</li>
-                  ))}
-                  <li>Price: {i.price}</li>
-                </ul>
-              ))}
-            </ul>
-          ))}
-        </ul>
-      </div>
+      <Grid container direction='column' gap={2} className='mainContainer'>
+        <Grid
+          item
+          container
+          direction='column'
+          className='containerLeft'
+          gap={2}
+        >
+          <Grid
+            item
+            direction='row'
+            className='titleHeader'
+            sm={2}
+            md={2}
+            lg={2}
+            xl={2}
+            flexGrow={1}
+            gap={2}
+          >
+            Pricing.....
+          </Grid>
+          <Grid item>Image</Grid>
+        </Grid>
+
+        <Grid
+          container
+          item
+          sm={4}
+          xl={4}
+          lg={4}
+          md={4}
+          className='containerRight'
+          direction='column'
+        >
+          <div className='subTitle'>Private Event Only </div>
+          <div>(Includes personalized digital invite)</div>
+          <Grid item>
+            {pricingInfo.map((info) => (
+              <ul key={info.id}>
+                <li>title{info.title}</li>
+                {info.info.map((i) => (
+                  <ul>
+                    <li>subTitle: {i.subTitle} </li>
+                    {i.details.map((deets) => (
+                      <li>{deets}</li>
+                    ))}
+                    <li>Price: {i.price}</li>
+                  </ul>
+                ))}
+              </ul>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     );
   }
 }
