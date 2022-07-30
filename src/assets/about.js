99676144216246e0df0d8a257d3bfb1da4e78c50
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-07-30 15:23:35 -0600

    Add page contents to a file

---
+++ src/assets/about.js
@@ -0,0 +1,27 @@
+const howItWorks = [
+  {
+    title: 'How it works',
+    intro:
+      'We are a coven of three witches that bring our magickal services to you to make your party or gathering a mystical event.',
+    steps: [
+      {
+        id: 1,
+        step: 'First…book your date and choose your add-ons.Pay your deposit.',
+      },
+      {
+        id: 2,
+        step: 'Next…send out your invites. Select digital or printed.',
+      },
+      {
+        id: 3,
+        step: 'Then make your final payment.',
+      },
+      {
+        id: 4,
+        step: 'We show up at your venue an hour before your event to transform our areas so that when your guests are experiencing their service, they feel surrounded by the mysticism of the Occult',
+      },
+    ],
+  },
+];
+
+export default howItWorks;
