68f46e2fb78ae499ee6017f65317550872f6a111
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-08-07 10:32:41 -0600

    Initial commit

---
+++ src/assets/covenMembers.js
@@ -0,0 +1,30 @@
+const covenMembers = [
+  {
+    title: 'Coven Members',
+    member: [
+      {
+        id: 1,
+        name: 'Christina',
+        title: 'Certified Reiki Heale and practicing Witch',
+        details:
+          'Service includes: Chakra scan and reading Energy healing session',
+      },
+      {
+        id: 2,
+        name: 'Stephanie',
+        title: 'Practicing Witch',
+        details:
+          'Service includes guests’ choice of: Enlightened Oracle or Tarot, dream Interpretation or palm reading',
+      },
+      {
+        id: 3,
+        name: 'Vincent',
+        title: 'Certified Yogi and practicing Witch',
+        details: 'Service includes: Personalized Yoga and breathing techniques for emotional channeling and healing'
+      },
+    ],
+    footer: '....All services are for entertainment purposes only....'
+  },
+];
+
+export default covenMembers;
