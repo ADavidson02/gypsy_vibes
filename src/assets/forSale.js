d0cf1bc93bbc5b30c32a5cc18969205a4004991d
Amanda Davidson <AmandaADavidson@hotmail.com>
2022-08-13 09:45:33 -0600

    Split items into seperate file

---
+++ src/assets/forSale.js
@@ -0,0 +1,93 @@
+const forSale = [
+  {
+    pageTitle: 'Items for sale at party',
+    id: 3,
+    vendors: [
+      {
+        name: 'Stephanie',
+        items: [
+          {
+            id: 1,
+            product: 'Party Girls',
+            price: '45',
+            description:
+              '27oz margarita glass, Yankee candle, wrinkle scarf, Fat Mama margarita mix',
+          },
+          {
+            id: 2,
+            product: 'Party Girls Duex',
+            price: '80',
+            description:
+              '56oz margarita glass, 2 Yankee candles, 2 wrinkle scarves, 2 Fat Mama margarita mix',
+          },
+          {
+            id: 3,
+            product: 'Tea Totaler',
+            price: '60',
+            description:
+              'Tea pot, slate tray, infuser, Metropolitan Tea (loose leaf and bagged)',
+          },
+          {
+            id: 4,
+            product: '2 for Tea Totaler',
+            price: '80',
+            description:
+              'Tea pot, 2 matching cups, slate tray, infuser, Metropolitan Tea (loose leaf and bagged)',
+          },
+          {
+            id: 5,
+            product: 'Souvenir Ornament',
+            price: '5 per item',
+            description:
+              'A Goddess sigil to bring a little magick into your life.',
+          },
+          {
+            id: 6,
+            product: 'Wallet Wishes ',
+            price: '15 per item',
+            description:
+              'Tiny spells for love, courage, or money you carry in your  purse or wallet',
+          },
+          {
+            id: 7,
+            product: 'Wine Bottle Cover',
+            price: 'TBD',
+            description: '',
+          },
+          {
+            id: 8,
+            product: 'Wiccan Rede',
+            price: 'TBD',
+            description: 'Coming Soon',
+          },
+        ],
+      },
+      {
+        name: 'Christina',
+        items: [
+          {
+            id: 1,
+            product: 'Chakra Bracelets set of 2',
+            price: '15',
+            description:
+              'For Enhanced meditation, Triple protection, Love vibrations, stress and anxiety relief',
+          },
+          {
+            id: 2,
+            product: 'Chakra Bracelets singles',
+            price: '10',
+            description: '',
+          },
+          {
+            id: 3,
+            product: 'Chakra Bags',
+            price: '15',
+            description: '7 large stones representing each chakra',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default forSale;
